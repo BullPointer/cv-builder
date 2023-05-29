e1457bc41edc544aa04333ae430eb4a8bba9b424
White Bull <blueded88@gmail.com>
2023-05-29 11:05:54 -0400

    Fixing some errors with MIME

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // plugins: [react()],
   plugins: [
-      react(),
+    react(),
     // Other plugins...
     {
       configureServer(server) {
