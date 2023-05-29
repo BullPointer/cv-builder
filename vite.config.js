f07970ccd47e1b73b0bd7b01bee8c2069a696d66
White Bull <blueded88@gmail.com>
2023-05-29 10:32:18 -0400

    Fixing some errors with MIME

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
+  // plugins: [react()],
+  plugins: [
+      react(),
+    // Other plugins...
+    {
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/main.js') {
+            res.setHeader('Content-Type', 'text/javascript');
+          }
+          next();
+        });
+      },
+    },
+  ],
 })
